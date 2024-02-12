4a400d6482003c23ac30b3a0bd723af95ff7072a
RennieFlc <105913262+RennieFlc@users.noreply.github.com>
2024-02-13 01:08:12 +0300

    Added Donation-Process-page/additional firebase backend

--- lib/pages/donation_history.dart
+++ lib/pages/donation_history.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
 
+import '../services/donation_service.dart';
+
 
 class DonationHistory extends StatelessWidget {
+  final DonationService donationService = DonationService();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Donation History'),
+      ),
+      body: StreamBuilder<List<Donation>>(
+        stream: donationService.getDonations(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
 
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final donations = snapshot.data!;
+          return ListView.builder(
+            itemCount: donations.length,
+            itemBuilder: (context, index) {
+              final donation = donations[index];
+              return ListTile(
+                title: Text(donation.description),
+                subtitle: Text('Date: ${donation.timestamp}'),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
 }
--- lib/pages/donation_process_page.dart
+++ lib/pages/donation_process_page.dart
@@ -1,10 +1,169 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class DonationProcessPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  _DonationProcessPageState createState() => _DonationProcessPageState();
+}
+
+class _DonationProcessPageState extends State<DonationProcessPage> {
+  String selectedClothingCategory = '';
+  String selectedCargoBrand = '';
+  String? imagePath;
+  String? shippingCode;
+
+
+  Future<void> _uploadImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      try {
+        final Reference ref = FirebaseStorage.instance
+            .ref()
+            .child('images')
+            .child('donations')
+            .child('image_${DateTime.now().millisecondsSinceEpoch}.jpg');
+        await ref.putFile(File(image.path)); // Uploading the image file to Firebase Storage!!
+        final String downloadURL = await ref.getDownloadURL(); // URL
+        setState(() {
+          imagePath = downloadURL; // URL AGAIN
+        });
+      } catch (e) {
+        print('Error uploading image: $e');
+      }
+    }
   }
 
+  void _handleSubmit() {
+    if (imagePath == null) {
+      // Show error message if picture is not uploaded!!!
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please upload a picture of the clothing item.'),
+        ),
+      );
+      return;
+    }
+
+    if (selectedClothingCategory.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please select a clothing category.'),
+        ),
+      );
+      return;
+    }
+
+    if(selectedCargoBrand.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please select your desired cargo company.'),
+        ),
+      );
+      return;
+    }
+
+    final Random random = Random();
+    shippingCode = 'SHIP${random.nextInt(10000)}'; // Assign generated shipping code
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Thank you for your donation! Your shipping code is: $shippingCode'),
+        duration: Duration(seconds: 10),
+      ),
+    );
+
+
+    Navigator.pop(context, shippingCode);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Donation Process'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFD9C9FF),
+              Color(0xFFB2E2E2),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              DropdownButtonFormField<String>(
+                value: selectedClothingCategory,
+                hint: Text('Select Clothing Category'),
+                onChanged: (value) {
+                  setState(() {
+                    selectedClothingCategory = value!;
+                  });
+                },
+                items: <String>[
+                  '',
+                  'Shirt',
+                  'Pants',
+                  'Dress',
+                  'Jacket',
+                  'Sweater',
+                  'Coat',
+                  'Gloves',
+                  'Hat',
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value == '' ? 'Select Clothing Category' : value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 150),
+              ElevatedButton(
+                onPressed: _uploadImage,
+                child: Text('Upload Clothing Picture'),
+              ),
+              SizedBox(height: 150),
+
+              if (imagePath != null) Image.network(imagePath!),
+              DropdownButtonFormField<String>(
+                value: selectedCargoBrand,
+                hint: Text('Select Cargo Company'),
+                onChanged: (value) {
+                  setState(() {
+                    selectedCargoBrand = value!;
+                  });
+                },
+                items: <String>[
+                  '',
+                  'Yurtiçi Kargo',
+                  'MNG Kargo',
+                  'Aras Kargo',
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value == '' ? 'Select Cargo Company' : value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 150),
+              ElevatedButton(
+                onPressed: _handleSubmit,
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
